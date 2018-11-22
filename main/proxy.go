056307778e00344181a038544226d49dac2d5990
madhan <madhan.s@freshdesk.com>
2018-11-22 23:48:06 +0530

    Addressed invalid targets, modified log format, etc

--- main/proxy.go
+++ main/proxy.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"github.com/gorilla/mux"
 	"gopkg.in/gcfg.v1"
 	"log"
@@ -19,8 +18,8 @@ const appEndPoint = "localhost:9000"
 type ServiceConfig struct {
 	RestartCmd      string
 	HealthCheckUrl  string
-	ServiceEndpoint string
-	ServiceDomain   string
+	ServiceTargetEndpoint string
+	ServiceLocalDomain   string
 	LogPath         string
 }
 type Config struct {
@@ -31,9 +30,10 @@ type Config struct {
 
 // Proxy section starts here
 func getTargetHost(req *http.Request) (*url.URL, error) {
+	host := strings.Split(req.Host,":")[0]
 	for _, v := range config.Service {
-		if req.Host == v.ServiceDomain {
-			return url.Parse(v.ServiceEndpoint)
+		if host == v.ServiceLocalDomain {
+			return url.Parse(v.ServiceTargetEndpoint)
 		}
 	}
 	return url.Parse(appEndPoint)
@@ -46,12 +46,16 @@ type proxyTransport struct {
 func (t *proxyTransport) RoundTrip(request *http.Request) (*http.Response, error) {
 	response, err := http.DefaultTransport.RoundTrip(request)
 
-	if strings.Contains(response.Header.Get("Content-Type"), "application/json") {
+	if nil == response {
+		log.Print(strings.Join([]string{"ERROR Endpoint : ", request.URL.Host, " could be down"},""))
+	} else if strings.Contains(response.Header.Get("Content-Type"), "application/json") {
 		body, err := httputil.DumpResponse(response, true)
 		if err != nil {
 			return nil, err
 		}
-		log.Print(string(body))
+		log.Println("------------------------Start of Response-------------")
+		log.Println(string(body))
+		log.Println("------------------------End of Response-------------")
 	}
 	return response, err
 }
@@ -63,7 +67,9 @@ func appProxy(target *url.URL) *httputil.ReverseProxy {
 			panic("error in the request dumping")
 		}
 		//todo have to set the host header from the config
-		fmt.Println(string(requestDump))
+		log.Println("------------------------Start of Request-------------")
+		log.Println(string(requestDump))
+		log.Println("------------------------End of Request-------------")
 		targetUrl, _ := getTargetHost(req)
 		req.URL.Scheme = targetUrl.Scheme
 		req.URL.Host = targetUrl.Host //config.Service["app"].ServiceEndpoint
@@ -86,5 +92,5 @@ func main() {
 	reverseProxy.Transport = &proxyTransport{} // Custom transport is needed for purpose like logging the response, measuring the latency etc
 	router.PathPrefix("/").Handler(reverseProxy);
 	error := http.ListenAndServe(appEndPoint, router)
-	fmt.Println(error)
+	log.Println(error)
 }
